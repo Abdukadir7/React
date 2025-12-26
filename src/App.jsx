17926d924b7528b24e94a370597d0297229c185d
Abdukadir7 <abdukadir270@gmail.com>
2025-12-26 19:14:48 +0300

    Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+    <div className="bg-blue-500 p-5">
+      <h1 className="text-3xl font-bold text-white ">
+        Tailwind is working!
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
