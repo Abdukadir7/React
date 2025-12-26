660fb274bf5e227dfcde473b21383d78a24d86d8
Abdukadir7 <abdukadir270@gmail.com>
2025-12-26 19:35:36 +0300

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css'
 
 function App() {
-
   return (
-    <>
-     
-    <div className="bg-blue-500 p-5">
-      <h1 className="text-3xl font-bold text-white ">
-        Tailwind is working!
-      </h1>
+    <div className="bg-blue-500 p-5 flex flex-row justify-start w-50 h-screen">
+      <div className="side-bar-branding text-white mr-10">
+        <ul className="space-y-4">
+          <li><a className='text-white hover:text-gray-300' href="#">Home</a></li>
+          <li><a className='text-white hover:text-gray-300' href="#">About</a></li>
+        </ul>
+      </div>
     </div>
-    </>
   )
 }
 
