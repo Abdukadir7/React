f8b33229340851e136ed6fee7839262733b05d70
Abdukadir7 <abdukadir270@gmail.com>
2025-12-28 20:43:57 +0300

    Attendence Rate

--- src/components/Attendence.jsx
+++ src/components/Attendence.jsx
@@ -3,7 +3,7 @@ const Attendence = () => {
   return (
   <div>
     <h1 className="text-2xl font-bold mb-4 text-gray-700">Student Attendence</h1>
-    
+    <h1>Attendence Rate</h1>
   </div>
   );
 };
